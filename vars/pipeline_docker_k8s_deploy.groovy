e847b3e19dc4682afb4987f8d622786b939eb1a8
Renato Mendoza <renatomendoza562@gmail.com>
2023-04-14 18:39:30 -0500

    first commit

---
+++ vars/pipeline_docker_k8s_deploy.groovy
@@ -0,0 +1,22 @@
+def call(body) {
+    def agent = 'any'
+    pipeline {
+        agent ${agent}
+        }
+        stages {
+            stage('Docker build') {
+                steps {
+                    script {
+                        println("Test print")
+                        println(testvar1)
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
