6c0c345efa12209a3cb87ff572a1cb1fe17f87fd
Renato Mendoza <renatomendoza562@gmail.com>
2023-04-15 17:20:37 -0500

    Method call udpate

--- vars/utility_buildAndPushDockerImage.groovy
+++ vars/utility_buildAndPushDockerImage.groovy
@@ -5,7 +5,7 @@ def call(opts) {
             sh "echo \${DOCKERHUB_PASSWORD} | docker login --username=\${DOCKERHUB_USERNAME} --password-stdin"
 
             // Build and push the Docker image
-            sh "docker build -t ${opts.dockerImageName}:${opts.dockerImageTag} -f ${WORKSPACE}/${opts.dockerfilePath} ."
+            sh "docker build -t ${opts.dockerRegistry}/${opts.dockerImageName}:${opts.dockerImageTag} -f ${WORKSPACE}/${opts.dockerfilePath} ."
             sh "docker push ${opts.dockerRegistry}/${opts.dockerImageName}:${opts.dockerImageTag}"
 
         }
