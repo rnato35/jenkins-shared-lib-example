e95e56cc16a516a3b5024de882f619ed41da2736
Renato Mendoza <renatomendoza562@gmail.com>
2023-04-14 21:32:36 -0500

    update

--- vars/utility_buildAndPushDockerImage.groovy
+++ vars/utility_buildAndPushDockerImage.groovy
@@ -2,7 +2,7 @@ def call(String imageName, String imageTag, String dockerfilePath) {
     withCredentials([usernamePassword(credentialsId: 'dockerhub-id', usernameVariable: 'DOCKERHUB_USERNAME', passwordVariable: 'DOCKERHUB_PASSWORD')]) {
         script {
             // Authenticate with Docker Hub
-            sh "echo \${DOCKER_PASSWORD} | docker login --username=\${DOCKER_USERNAME} --password-stdin"
+            sh "echo \${DOCKERHUB_PASSWORD} | docker login --username=\${DOCKERHUB_USERNAME} --password-stdin"
 
             // Build and push the Docker image
             sh "docker build -t ${imageName}:${imageTag} -f ${dockerfilePath} ."
