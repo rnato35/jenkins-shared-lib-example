b37b300b30382f2a3f38dfd3382d4c5a39db5bca
Renato Mendoza <renatomendoza562@gmail.com>
2023-04-15 17:18:08 -0500

    Registry update

--- vars/utility_buildAndPushDockerImage.groovy
+++ vars/utility_buildAndPushDockerImage.groovy
@@ -1,14 +1,12 @@
-def call(String imageName, String imageTag, String dockerfilePath) {
+def call(opts) {
     withCredentials([usernamePassword(credentialsId: 'dockerhub-id', usernameVariable: 'DOCKERHUB_USERNAME', passwordVariable: 'DOCKERHUB_PASSWORD')]) {
         script {
             // Authenticate with Docker Hub
             sh "echo \${DOCKERHUB_PASSWORD} | docker login --username=\${DOCKERHUB_USERNAME} --password-stdin"
 
-                        sh "ls -ltra && pwd && echo \${WORKSPACE}"
-
             // Build and push the Docker image
-            sh "docker build -t ${imageName}:${imageTag} -f ${WORKSPACE}${dockerfilePath} ."
-            sh "docker push ${imageName}:${imageTag}"
+            sh "docker build -t ${opts.dockerImageName}:${opts.dockerImageTag} -f ${WORKSPACE}/${opts.dockerfilePath} ."
+            sh "docker push ${opts.dockerRegistry}/${opts.dockerImageName}:${opts.dockerImageTag}"
 
         }
     }
