852b5be2cb5535b2e1fcc86b1f34251e97885eda
Renato Mendoza <renatomendoza562@gmail.com>
2023-04-15 17:25:40 -0500

    Method call udpate

--- vars/utility_buildAndPushDockerImage.groovy
+++ vars/utility_buildAndPushDockerImage.groovy
@@ -1,12 +1,18 @@
 def call(opts) {
     withCredentials([usernamePassword(credentialsId: 'dockerhub-id', usernameVariable: 'DOCKERHUB_USERNAME', passwordVariable: 'DOCKERHUB_PASSWORD')]) {
         script {
-            // Authenticate with Docker Hub
+            /* // Authenticate with Docker Hub
             sh "echo \${DOCKERHUB_PASSWORD} | docker login --username=\${DOCKERHUB_USERNAME} --password-stdin"
 
             // Build and push the Docker image
             sh "docker build -t ${opts.dockerRegistry}/${opts.dockerImageName}:${opts.dockerImageTag} -f ${WORKSPACE}/${opts.dockerfilePath} ."
-            sh "docker push ${opts.dockerRegistry}/${opts.dockerImageName}:${opts.dockerImageTag}"
+            sh "docker push ${opts.dockerRegistry}/${opts.dockerImageName}:${opts.dockerImageTag}" */
+
+            def dockerImage = docker.build("${opts.dockerRegistry}/${opts.dockerImageName}:${opts.dockerImageTag}")
+
+            docker.withRegistry("https://registry.hub.docker.com", DOCKERHUB_USERNAME, DOCKERHUB_PASSWORD) {
+                dockerImage.push()
+}
 
         }
     }
